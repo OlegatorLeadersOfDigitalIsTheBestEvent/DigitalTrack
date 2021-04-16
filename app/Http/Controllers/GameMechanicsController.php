55715ee4c664c5a3769d898062bd31698e1906b9
root <root@a605555.fvds.ru>
2021-04-16 23:22:36 +0300

    Auth and controllers

---
+++ app/Http/Controllers/GameMechanicsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+
+use Illuminate\Http\Request;
+use App\Teams;
+use App\PublicNews;
+use App\PrivateNews;
+use App\Cards;
+use App\SelectedCards;
+use App\ActiveCards;
+use App\StepCards;
+use App\ClientsEmail;
+
+
+use Mail;
+use Validator;
+
+class GameMechanicsController extends Controller{
+
+}
